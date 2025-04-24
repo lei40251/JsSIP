08e881d3c1d3e5ea34fc21ae3bf324610a78a29d
路人乙 <lei40251@163.com>
2025-04-24 14:08:25 +0800

    兼容Firefox，关闭bundle，获取本端流过滤canvas调整

--- dist/CRTC.js
+++ dist/CRTC.js
@@ -1,5 +1,5 @@
 /*
- * CRTC v1.10.9-beta.20254241337
+ * CRTC v1.10.9-beta.2025424141
  * the Javascript WebRTC and SIP library
  * Copyright: 2012-2025 
  */
@@ -3538,7 +3538,7 @@ exports.load = function (dst, src) {
 "use strict";
 
 module.exports = {
-  USER_AGENT: 'UA/1.10.9-beta.405008482674 (Web)',
+  USER_AGENT: 'UA/1.10.9-beta.405008482802 (Web)',
   // SIP scheme.
   SIP: 'sip',
   SIPS: 'sips',
@@ -16832,7 +16832,7 @@ var WebSocketInterface = require('./WebSocketInterface');
 var debug = require('debug')('CRTC');
 var getStats = require('./Stats');
 var BFCPLib = require('./BFCP');
-debug('version %s', '1.10.9-beta.405008482674');
+debug('version %s', '1.10.9-beta.405008482802');
 (function () {
   if (typeof window.CustomEvent === 'function') return;
   function CustomEvent(event, params) {
@@ -16869,7 +16869,7 @@ module.exports = {
     return 'CRTC';
   },
   get version() {
-    return '1.10.9-beta.405008482674';
+    return '1.10.9-beta.405008482802';
   }
 };
 },{"./BFCP":1,"./Constants":32,"./Exceptions":36,"./Grammar":37,"./NameAddrHeader":41,"./Stats":54,"./UA":58,"./URI":59,"./Utils":60,"./WebSocketInterface":61,"debug":66}],39:[function(require,module,exports){
@@ -20671,6 +20671,10 @@ module.exports = /*#__PURE__*/function (_EventEmitter) {
 
         // 非BFCP修改为只支持 42c01e
         _this18._enableBFCP || (desc.sdp = desc.sdp.replace(/42e01f/g, '42c01e'));
+
+        // 兼容 Firefox 去掉 bundle
+        desc.sdp = desc.sdp.replace(/a=bundle-only\r\n/g, '');
+        desc.sdp = desc.sdp.replace(/m=video 0 /g, 'm=video 9 ');
         return connection.setLocalDescription(desc)["catch"](function (error) {
           _this18._rtcReady = true;
           logger.warn('emit "peerconnection:setlocaldescriptionfailed" [error:%o]', error);
@@ -22014,6 +22018,7 @@ module.exports = /*#__PURE__*/function (_EventEmitter) {
                   }
                 }, _callee6);
               })))["catch"](function (error) {
+                console.warn(error);
                 _this30._acceptAndTerminate(response, 488, 'Not Acceptable Here');
                 _this30._failed('remote', response, CRTC_C.causes.BAD_MEDIA_DESCRIPTION);
                 logger.warn('emit "peerconnection:setremotedescriptionfailed" [error:%o]', error);
@@ -28891,8 +28896,15 @@ exports.getStreams = function (pc, type, i) {
           if (sender.track && sender.track.readyState === 'live') {
             if (sender.track.kind === 'audio') {
               audioStream.addTrack(sender.track);
-            } else if (!(sender.track instanceof CanvasCaptureMediaStreamTrack)) {
-              videoStream.addTrack(sender.track);
+            } else {
+              // 检查是否为Canvas轨道的兼容方案
+              var isCanvasTrack = function isCanvasTrack() {
+                var settings = sender.track.getSettings();
+                return settings.deviceId === 'canvas' || sender.track.label.toLowerCase().includes('canvas') || sender.track.constructor && sender.track.constructor.name === 'CanvasCaptureMediaStreamTrack' || sender.track.kind === 'video' && sender.track.contentHint === 'drawing';
+              };
+              if (!isCanvasTrack()) {
+                videoStream.addTrack(sender.track);
+              }
             }
           }
         });
