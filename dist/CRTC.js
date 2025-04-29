a6fb683cbcffbf7c8be0699640759d6881b5d2c1
路人乙 <lei40251@163.com>
2025-04-29 18:30:53 +0800

    适配方法优化

--- dist/CRTC.js
+++ dist/CRTC.js
@@ -1,5 +1,5 @@
 /*
- * CRTC v1.10.9-beta.20254291651
+ * CRTC v1.10.9-beta.2025429175
  * the Javascript WebRTC and SIP library
  * Copyright: 2012-2025 
  */
@@ -3538,7 +3538,7 @@ exports.load = function (dst, src) {
 "use strict";
 
 module.exports = {
-  USER_AGENT: 'UA/1.10.9-beta.405008583302 (Web)',
+  USER_AGENT: 'UA/1.10.9-beta.405008583410 (Web)',
   // SIP scheme.
   SIP: 'sip',
   SIPS: 'sips',
@@ -16832,7 +16832,7 @@ var WebSocketInterface = require('./WebSocketInterface');
 var debug = require('debug')('CRTC');
 var getStats = require('./Stats');
 var BFCPLib = require('./BFCP');
-debug('version %s', '1.10.9-beta.405008583302');
+debug('version %s', '1.10.9-beta.405008583410');
 (function () {
   if (typeof window.CustomEvent === 'function') return;
   function CustomEvent(event, params) {
@@ -16869,7 +16869,7 @@ module.exports = {
     return 'CRTC';
   },
   get version() {
-    return '1.10.9-beta.405008583302';
+    return '1.10.9-beta.405008583410';
   }
 };
 },{"./BFCP":1,"./Constants":32,"./Exceptions":36,"./Grammar":37,"./NameAddrHeader":41,"./Stats":54,"./UA":58,"./URI":59,"./Utils":60,"./WebSocketInterface":61,"debug":66}],39:[function(require,module,exports){
@@ -22585,7 +22585,7 @@ module.exports = /*#__PURE__*/function (_EventEmitter) {
 
       // safari 等场景ended 或者 inactive 事件不会触发
       var timer = setInterval(function () {
-        if (_this35._localShareStream.getVideoTracks()[0].readyState === 'ended') {
+        if (_this35._localShareStream && _this35._localShareStream.getVideoTracks() && _this35._localShareStream.getVideoTracks()[0].readyState === 'ended') {
           clearInterval(timer);
           ended || mediaStreamTrackEndedHandler();
           ended = true;
