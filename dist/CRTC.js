47997cc933e20844495c694e1c65759fffd04f2e
赵磊 <zhaol@cloudunicomm.com>
2025-08-29 14:54:33 +0800

    增加DC乱序，不可靠参数

--- dist/CRTC.js
+++ dist/CRTC.js
@@ -1,5 +1,5 @@
 /*
- * CRTC v1.10.11-beta.2025829111
+ * CRTC v1.10.11-beta.20258291454
  * the Javascript WebRTC and SIP library
  * Copyright: 2012-2025 
  */
@@ -3539,7 +3539,7 @@ exports.load = function (dst, src) {
 "use strict";
 
 module.exports = {
-  USER_AGENT: 'UA/1.10.11-beta.405016582202 (Web)',
+  USER_AGENT: 'UA/1.10.11-beta.405016582908 (Web)',
   // SIP scheme.
   SIP: 'sip',
   SIPS: 'sips',
@@ -16851,7 +16851,7 @@ var debug = require('debug')('CRTC');
 var getStats = require('./Stats');
 var BFCPLib = require('./BFCP');
 var Mixer = require('./Mixer');
-debug('version %s', '1.10.11-beta.405016582202');
+debug('version %s', '1.10.11-beta.405016582908');
 (function () {
   if (typeof window.CustomEvent === 'function') return;
   function CustomEvent(event, params) {
@@ -16889,7 +16889,7 @@ module.exports = {
     return 'CRTC';
   },
   get version() {
-    return '1.10.11-beta.405016582202';
+    return '1.10.11-beta.405016582908';
   }
 };
 },{"./BFCP":1,"./Constants":32,"./Exceptions":36,"./Grammar":37,"./Mixer":41,"./NameAddrHeader":42,"./Stats":55,"./UA":59,"./URI":60,"./Utils":61,"./WebSocketInterface":62,"debug":67}],39:[function(require,module,exports){
@@ -18222,7 +18222,10 @@ module.exports = /*#__PURE__*/function (_EventEmitter) {
     _this._dataChannel = null;
     _this._dataChannelName = CRTC_C.BFCP;
     _this._dataChannelReady = false;
-    _this._dataChannelConfig = {};
+    _this._dataChannelConfig = {
+      ordered: false,
+      maxRetransmits: 0
+    }; // 乱序，不可靠
     _this._dataChannelMsgs = {};
 
     // 是否启用BFCP
