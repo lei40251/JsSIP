a202adcaf7002a8e8bd39b5e90f569d87ea5426f
Jett <lei40251@163.com>
2025-09-20 22:40:56 +0800

    group bundle

--- dist/CRTC.js
+++ dist/CRTC.js
@@ -1,5 +1,5 @@
 /*
- * CRTC v1.11.2.20259202045
+ * CRTC v1.11.2.20259202235
  * the Javascript WebRTC and SIP library
  * Copyright: 2012-2025 
  */
@@ -3539,7 +3539,7 @@ exports.load = function (dst, src) {
 "use strict";
 
 module.exports = {
-  USER_AGENT: 'UA/1.11.2.405018404090 (Web)',
+  USER_AGENT: 'UA/1.11.2.405018404470 (Web)',
   // SIP scheme.
   SIP: 'sip',
   SIPS: 'sips',
@@ -16851,7 +16851,7 @@ var debug = require('debug')('CRTC');
 var getStats = require('./Stats');
 var BFCPLib = require('./BFCP');
 var Mixer = require('./Mixer');
-debug('version %s', '1.11.2.405018404090');
+debug('version %s', '1.11.2.405018404470');
 (function () {
   if (typeof window.CustomEvent === 'function') return;
   function CustomEvent(event, params) {
@@ -16889,7 +16889,7 @@ module.exports = {
     return 'CRTC';
   },
   get version() {
-    return '1.11.2.405018404090';
+    return '1.11.2.405018404470';
   }
 };
 },{"./BFCP":1,"./Constants":32,"./Exceptions":36,"./Grammar":37,"./Mixer":41,"./NameAddrHeader":42,"./Stats":55,"./UA":59,"./URI":60,"./Utils":61,"./WebSocketInterface":62,"debug":67}],39:[function(require,module,exports){
@@ -27522,7 +27522,7 @@ module.exports = /*#__PURE__*/function () {
 
       // 统一修改发出的SDP
       message = Utils.processSdp(message);
-      // message = message.replace(/a=group:BUNDLE.*\r\n/, '');
+      message = message.replace(/a=group:BUNDLE.*\r\n/, '');
       message = message.replace(/a=candidate.*typ host.*\r?\n/gm, '');
       // 去掉IPV6
       message = message.replace(/a=candidate:.*:.*\r\n/g, '');
