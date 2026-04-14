e9c893b3a4c7cd460692a6987a62313fc8d440a4
赵磊 <zhaol@cloudunicomm.com>
2026-04-14 20:33:29 +0800

    v1.12.1

--- dist/CRTC.js
+++ dist/CRTC.js
@@ -1,5 +1,5 @@
 /*
- * CRTC v1.12.1-beta.20264131754
+ * CRTC v1.12.1.2026414204
  * the Javascript WebRTC and SIP library
  * Copyright: 2012-2026 
  */
@@ -3539,7 +3539,7 @@ exports.load = function (dst, src) {
 "use strict";
 
 module.exports = {
-  USER_AGENT: 'UA/1.12.1-beta.405208263508 (Web)',
+  USER_AGENT: 'UA/1.12.1.405208284008 (Web)',
   // SIP scheme.
   SIP: 'sip',
   SIPS: 'sips',
@@ -16854,7 +16854,7 @@ var getStats = require('./Stats');
 var BFCPLib = require('./BFCP');
 var Mixer = require('./Mixer');
 var VirtualBackground = require('./VirtualBackground/index.js');
-debug('version %s', '1.12.1-beta.405208263508');
+debug('version %s', '1.12.1.405208284008');
 (function () {
   if (typeof window.CustomEvent === 'function') return;
   function CustomEvent(event, params) {
@@ -16893,7 +16893,7 @@ module.exports = {
     return 'CRTC';
   },
   get version() {
-    return '1.12.1-beta.405208263508';
+    return '1.12.1.405208284008';
   }
 };
 },{"./BFCP":1,"./Constants":32,"./Exceptions":36,"./Grammar":37,"./Mixer":41,"./NameAddrHeader":42,"./Stats":55,"./UA":59,"./URI":60,"./Utils":61,"./VirtualBackground/index.js":63,"./WebSocketInterface":71,"debug":76}],39:[function(require,module,exports){
