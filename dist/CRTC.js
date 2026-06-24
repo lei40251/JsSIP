c8df3cf226a0310434685e09cf317dbeadeff195
赵磊 <zhaol@cloudunicomm.com>
2026-06-24 14:07:11 +0800

    增加DC  AS 500

--- dist/CRTC.js
+++ dist/CRTC.js
@@ -1,5 +1,5 @@
 /*
- * CRTC v1.13.3.20266151557
+ * CRTC v1.13.3.2026624145
  * the Javascript WebRTC and SIP library
  * Copyright: 2012-2026 
  */
@@ -3539,7 +3539,7 @@ exports.load = function (dst, src) {
 "use strict";
 
 module.exports = {
-  USER_AGENT: 'UA/1.13.3.405212303114 (Web)',
+  USER_AGENT: 'UA/1.13.3.405212482810 (Web)',
   // SIP scheme.
   SIP: 'sip',
   SIPS: 'sips',
@@ -16854,7 +16854,7 @@ var getStats = require('./Stats');
 var BFCPLib = require('./BFCP');
 var Mixer = require('./Mixer');
 var VirtualBackground = require('./VirtualBackground/index.js');
-debug('version %s', '1.13.3.405212303114');
+debug('version %s', '1.13.3.405212482810');
 (function () {
   if (typeof window.CustomEvent === 'function') return;
   function CustomEvent(event, params) {
@@ -16893,7 +16893,7 @@ module.exports = {
     return 'CRTC';
   },
   get version() {
-    return '1.13.3.405212303114';
+    return '1.13.3.405212482810';
   }
 };
 },{"./BFCP":1,"./Constants":32,"./Exceptions":36,"./Grammar":37,"./Mixer":41,"./NameAddrHeader":42,"./Stats":55,"./UA":59,"./URI":60,"./Utils":61,"./VirtualBackground/index.js":63,"./WebSocketInterface":71,"debug":76}],39:[function(require,module,exports){
@@ -22135,6 +22135,12 @@ module.exports = /*#__PURE__*/function (_EventEmitter) {
                 limit: 2000
               }];
             }
+          } else {
+            _bandAS += 500;
+            media.bandwidth = [{
+              type: 'AS',
+              limit: 500
+            }];
           }
         });
 
